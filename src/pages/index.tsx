1944516bce90395ddbed7e6d237f9fd94236e8ca
Ingrid Larissa <larissaorcx@gmail.com>
2022-03-03 00:56:03 -0300

    Adicionando as funcionalidades na página index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,25 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Image = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+};
+
+interface FetchImgProps {
+  after?: string;
+  data: Image[];
+}
+
 export default function Home(): JSX.Element {
+  async function fetchImg({ pageParam = null }): Promise<FetchImgProps> {
+    const response = await api.get(`/api/images?after=${pageParam}`);
+    return response;
+  }
+
   const {
     data,
     isLoading,
@@ -16,20 +34,20 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImg, {
+    getNextPageParam: lastPage => lastPage?.after || null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data.pages.flatMap(dados => dados.data);
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
-
-  // TODO RENDER ERROR SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +55,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage} disable={isFetchingNextPage}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
